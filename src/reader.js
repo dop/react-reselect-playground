297691f409a464b1d91a9e156eadba21f5db7578
Donatas Petrauskas <donatasp@wix.com>
2017-11-30 14:07:11 +0200

    Remove code.

--- src/reader.js
+++ src/reader.js
@@ -56,72 +56,7 @@ class TableReader {
     }
     return people;
   }
-
-  getPeople2() {
-    return memoPeople(this.isPhoneVisible(), this.isEmailVisible(), this.getNames());
-  }
-
-  getFilteredPeople2() {
-    return memoFilteredPeople(this.getQuery(), this.getPeople2());
-  }
-
-  getPeople3() {
-    return peopleSelector(this.state);
-  }
-
-  getFilteredPeople3() {
-    return filteredPeopleSelector(this.state);
-  }
 }
-
-const peopleSelector = createSelector(
-  (state) => state.showPhone,
-  (state) => state.showEmail,
-  (state) => state.names,
-  (isPhoneVisible, isEmailVisible, names) => {
-    return names.map(({name, contact, memberSince}) => {
-      const {first, last} = name;
-      const {phone, email} = contact;
-      return {
-        name: first + ' ' + last,
-        since: memberSince,
-        phone: isPhoneVisible ? phone[0] : '',
-        email: isEmailVisible ? email[0] : ''
-      };
-    });
-  }
-);
-
-const filteredPeopleSelector = createSelector(
-  (state) => state.query,
-  (state) => peopleSelector(state),
-  (query, people) => {
-    if (query) {
-      return people.filter((person) => person.name.toLowerCase().includes(query.toLowerCase()));
-    }
-    return people;
-  }
-);
-
-const memoPeople = defaultMemoize(function (isPhoneVisible, isEmailVisible, names) {
-  return names.map(({name, contact, memberSince}) => {
-    const {first, last} = name;
-    const {phone, email} = contact;
-    return {
-      name: first + ' ' + last,
-      since: memberSince,
-      phone: isPhoneVisible ? phone[0] : '',
-      email: isEmailVisible ? email[0] : ''
-    };
-  });
-});
-
-const memoFilteredPeople = defaultMemoize(function (query, people) {
-  if (query) {
-    return people.filter((person) => person.name.toLowerCase().includes(query.toLowerCase()));
-  }
-  return people;
-});
 
 export function stateReader(state) {
   return {
